0459eebc3b2550140ec33cf7c757996f48e629d1
Nikolas Charalambidis <chan01@vse.cz>
2021-02-27 22:32:00 +0100

    Country fix

--- src/main/java/com/nikolascharalambidis/interview/landroutes/data/Country.java
+++ src/main/java/com/nikolascharalambidis/interview/landroutes/data/Country.java
@@ -4,15 +4,15 @@ import java.util.List;
 import java.util.Objects;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class Country {
 
-	private String name;
-	private Region region;
-	private List<String> borders;
+	private final String name;
+	private final Region region;
+	private final List<String> borders;
 
 	@Override
 	public boolean equals(final Object o) {
